c1285bf87940b2197e3928ea51b163323d90226b
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-09-28 12:01:17 +0100

    Functions topic code added

--- basics.js
+++ basics.js
@@ -99,7 +99,7 @@ if(temp<20){
     console.log("do nothing")
 }
 
-//switch cases
+//Switch cases - use when you have a set number of conditions
 let day = "tues";
 switch(day){
     case "mon":
@@ -114,6 +114,127 @@ switch(day){
     case "thurs":
         console.log("Day 4");
         break;
+    case "fri":
+        console.log("Day 5");
+        break;
+    case "sat":
+        console.log("Day 6");
+        break;
+    case "sun":
+        console.log("Day 7");
+        break;
     default:
         console.log("default");
 }
+
+//Ternary statements
+// ternary operator - ?
+// syntax:  condition ? valueIfTrue : valueIfFalse
+let age = 10;
+
+if(age>=25){
+    console.log("can buy alco")
+}else{
+    console.log("can't buy alco")
+}
+
+let tern = age>=25 ? "can buy alco" : "can't buy alco";
+console.log(tern, "ternary");
+
+//Objects - defined as an unordered collection of related data (key:value pairs)
+let personObj = {
+    name: "ash",
+    age: 12,
+    job: "trainer"
+}
+console.log("person:", personObj)
+console.log(JSON.stringify(personObj)); //method to take an object and convert it into a string
+
+//JSON - JavaScript Object Notation
+let myObj = `[{
+    "name": "ash",
+    "age": 12,
+    "job": "trainer"
+},
+{
+    "name": "JH",
+    "age": 24,
+    "job": "trainer"
+}]`;
+
+console.log("JSON", myObj);
+//Example of loading JSON DATA:
+//GET request
+// fetch("people.json")
+// .then(response => response.json())
+// .then(data => console.log("fetch", data));
+
+//Arrays
+const myArr = [1, "2", 3, 4, 5, 6, 7];
+const names = ["bob", "mickey", "eeyore", "minnie"];
+console.log(myArr);
+console.log(names);
+console.log(names[1]);//mickey
+names.push("jerry");// only way to modify constant variables is through a method
+console.log(names);
+
+//Destructuring
+const [a1, a2, a3, a4, a5, a6, a7] = myArr;
+console.log(myArr[0]+myArr[1]);
+console.log(a1+a2);// 1 + "2" = 12
+console.log(a1+parseInt(a2));// 1 + 2 = 3
+//spread operator - ...
+const[name1, name2, ...rest] = names;
+console.log(name2);
+console.log("rest:", rest);
+
+const grades = [21, 43, 54, 64, 23, 96, 27, 84, 100];
+const[,,,grade, ...others] = grades;
+console.log(grade);
+console.log(others);
+
+
+let myObj2 = {
+    name: "peppy",
+    breed: "chihuahua",
+    
+}
+console.log("initial",myObj2);
+const n = myObj2.name;
+console.log(n);
+
+myObj2 = {...myObj2, ...{size:"xs", colour:"brown"}};
+console.log("updated:", myObj2);
+
+
+//Funtions/methods - are a set of instructions you want to execute/ reuse
+//3 ways to write functions
+
+//function - global scope, call the funtion before it's been declared
+
+myFun("hello", "ash");
+myFun("normal function","hi");
+
+function myFun(p,x){
+    console.log(p);
+    console.log(p+" "+x);
+}
+
+//function expression - has to be initalised before called
+myFun2("function expression");
+const myFun2 = function(x){
+    console.log(x);
+}
+
+
+//arrow functions - more concise way of writing function expressions
+const myFun3 = (z) => console.log(z);
+
+const myFun4 = (t,u) => {
+    let sum = t + u;
+    console.log(t,"+",u,"=",sum);
+}
+
+myFun3("arrow function");// z = "arrow function"
+
+myFun4(3,4);// t= 3, u = 4
